02b4b07b3fe193db8bb88dea966095868a91e118
AliCarr <alastairrosscarr@gmail.com>
2020-02-06 16:31:32 +0000

    Pathfinding Alpha Done
    Current method for the enemy movement is based on trigger points that the player reaches, which then alter the direction. These can easily be placed, and have a drop down set of directions for the designers ease

--- DES505/Assets/Scripts/Behaviours/Enemy/EnemyMovement.cs
+++ DES505/Assets/Scripts/Behaviours/Enemy/EnemyMovement.cs
@@ -6,23 +6,17 @@ public enum DIRECTION { NORTH, SOUTH, EAST, WEST };
 
 public class EnemyMovement : MonoBehaviour
 {
- 
-    DIRECTION currentDirection = DIRECTION.SOUTH;
-    Vector2 movement = new Vector2 (0, -1);
+    private Vector2 movement = new Vector2(0, -1);
 
-    [Range(0.01f, 0.1f)]
+    [Range(0.1f, 10.0f)]
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public DIRECTION currentDirection;
 
-    // Update is called once per frame
+
     void Update()
     {
         UpdateDirection();
-        this.gameObject.transform.Translate(new Vector3(movement.x * speed, movement.y * speed, 0), Space.World);
+        this.gameObject.transform.Translate(new Vector3(movement.x * speed *Time.unscaledDeltaTime, movement.y * speed * Time.unscaledDeltaTime, 0), Space.World);
     }
 
     void UpdateDirection()
@@ -51,16 +45,14 @@ public class EnemyMovement : MonoBehaviour
         }
     }
 
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Turn Node")
-            currentDirection = DIRECTION.EAST;
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Turn Node")
             currentDirection = collision.GetComponent<Node>().setDirection;
+
+        if(collision.gameObject.tag == "Forest")
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
---
+++ DES505/Assets/Scripts/Behaviours/EnemyFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour
+{
+    public GameObject Enemy;
+    float creationRate = 1.3f;
+    int maxEnemies = 6;
+    int currentEnemyCount = 0;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= creationRate && transform.childCount <= maxEnemies)
+        {
+            Instantiate(Enemy, transform);
+            timer = 0;
+        }
+
+        //currentEnemyCount = transform.childCount;
+    }
+}
