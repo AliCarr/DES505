c8e7890ecf7105d9b1cab82fd4a3dae6ea7c2cd5
AliCarr <alastairrosscarr@gmail.com>
2020-02-17 16:42:28 +0000

    More Round Control
    Each round is now created as a data type called "Round". This stores the data needed to start a round (currently just the quantites of enemies by type) and then allows the factory to spawn that given number in a relativly random order.

--- DES505/Assets/Enemy/Scripts/EnemyFactory.cs
+++ DES505/Assets/Enemy/Scripts/EnemyFactory.cs
@@ -4,28 +4,75 @@ using UnityEngine;
 
 public class EnemyFactory : MonoBehaviour
 {
-    public GameObject Enemy;
-    public const int maxEnemies = 10;
     public const int pathMax = 3;
     public Transform[] waypoints;
+    public Round r;
+    public GameObject CO2EnemyObject;
+    public GameObject SF6EnemyObject;
+    public GameObject CH4EnemyObject;
 
     private float creationRate = 1.3f;
     private int currentEnemyCount = 0;
     private float timer = 0;
     private int enemiesSpawned = 0;
+    List<GameObject> spawnList = new List<GameObject>();
 
+
+    private void Start()
+    {
+        FillSpawnList();
+    }
     void Update()
     {
         //Start each frame by incrementing the timer
         timer += Time.deltaTime;
 
-        if(enemiesSpawned <= maxEnemies && timer >= creationRate && currentEnemyCount <= pathMax)
+     
+
+        if (enemiesSpawned <= r.GetTotal() && timer >= creationRate && currentEnemyCount <= pathMax)
         {
-            Instantiate(Enemy, transform).GetComponent<EnemyBehaviour>().SetWaypoints(waypoints);
+            Instantiate(CO2EnemyObject, transform).GetComponent<EnemyBehaviour>().SetWaypoints(waypoints);
             timer = 0;
             ++enemiesSpawned;
         }
 
         currentEnemyCount = transform.childCount;
+    }
+
+    void FillSpawnList()
+    {
+        for(int c = 0; c < r.CO2; ++c)
+        {
+            spawnList.Add(CO2EnemyObject);
+        }
+
+        for (int c = 0; c < r.SF6; ++c)
+        {
+            spawnList.Add(SF6EnemyObject);
+        }
+
+        for (int c = 0; c < r.CH4; ++c)
+        {
+            spawnList.Add(CH4EnemyObject);
+        }
+
+        spawnList = Shuffle(spawnList);
+
+    }
+
+    private List<GameObject> Shuffle(List<GameObject> input)
+    {
+        List<GameObject> output = new List<GameObject>();
+
+        Random r = new Random();
+        int randomIndex = 0;
+        while(input.Count > 0)
+        {
+            randomIndex = Random.Range(0, input.Count);
+            output.Add(input[randomIndex]);
+            input.RemoveAt(randomIndex);
+        }
+
+        return output;
     }
 }
--- DES505/Assets/Scriptable Objects/Rounds/Round.cs
+++ DES505/Assets/Scriptable Objects/Rounds/Round.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 [CreateAssetMenu]
 public class Round : ScriptableObject
 {
-    public int roundNumber;
-
     //How many CO2 enemies this round
     public int CO2;
 
     //How many SF6 enemies this round
     public int SF6;
-
+ 
     //How many CH4 enemies this round
     public int CH4;
+
+    public int GetTotal()
+    {
+        int total = CO2 + SF6 + CH4;
+        return total;
+    }
 }
